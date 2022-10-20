7110e3cec841a285e10bc6cb365bf0c6108b79f1
Daniel Sánchez <axis@thefutures.io>
2022-10-20 11:55:28 -0400

    test survey

--- middleware/test.js
+++ middleware/test.js
@@ -1,11 +1,16 @@
 export default async function ({ $axios, redirect }) {
-  try {
-    const response = await $axios.$get(
-      "https://gwy-dev.thefutures.io/gateway/tickets"
-    );
-  } catch (error) {
-    console.error("error", error);
-    redirect(401, "/");
-    console.log("redirected by middleware");
-  }
+  console.log("passin tru middleware");
+  // if (!localStorage.getItem("myCat")) {
+  //   redirect(401, "/");
+  // }
+  // localStorage.getItem("myCat");
+  // try {
+  //   const response = await $axios.$get(
+  //     "https://gwy-dev.thefutures.io/gateway/tickets"
+  //   );
+  // } catch (error) {
+  //   console.error("error", error);
+  //   redirect(401, "/");
+  //   console.log("redirected by middleware");
+  // }
 }
--- pages/testSurvey.vue
+++ pages/testSurvey.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
     <p>test survey</p>
+    <survey :survey="survey"> </survey>
   </div>
 </template>
 
 <script>
+import * as SurveyVue from "survey-vue";
+
+const { Survey } = SurveyVue;
+
+const surveyJson = {
+  elements: [
+    {
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text",
+    },
+    {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text",
+    },
+  ],
+};
+
 export default {
   name: "testSurvey",
-  middleware: "test",
+  data() {
+    const model = new SurveyVue.Model(surveyJson);
+
+    return {
+      survey: model,
+    };
+  },
 };
 </script>
 
